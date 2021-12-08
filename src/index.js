df82f1498276feb019da4eeb5101483719de78b4
Samindra <samindra.saha@jhorotek.com>
2021-12-08 17:23:12 +0600

    fonts and assets added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './fonts/Poppins-SemiBold.ttf';
+import './fonts/Poppins-Regular.ttf';
+import './fonts/Poppins-Medium.ttf';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
