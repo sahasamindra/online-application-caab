10f461f55cbd4c6c097b2cf91178847fbe824213
Samindra <samindra.saha@jhorotek.com>
2021-12-22 17:49:34 +0600

    footer

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -6,6 +6,10 @@ function Layout() {
   return (
     <div className="global">
       <Header />
+      <div className="footer">
+        Copyright @2021{" "}
+        <a href="/footer">Civil Aviation Authority of Bangladesh</a>
+      </div>
     </div>
   );
 }
