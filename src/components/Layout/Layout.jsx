cb4de47813f1a790519bb677c29e9e9997cebe15
Samindra <samindra.saha@jhorotek.com>
2021-12-23 13:54:32 +0600

    sidebar done

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,15 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header/Header";
 import "./layout.css";
 
 function Layout() {
+  let menus = ["Home", "New Application", "Settings"];
+  const [active, setActive] = useState(0);
+
   return (
     <div className="global">
-      <Header />
-      <div className="side-bar"></div>
+      <div className="header-bar">
+        <Header />
+      </div>
+
+      <div className="side-bar">
+        <div className="menu">
+          <div>
+            {menus.map((item, index) => (
+              <div
+                className={`menu-item ${
+                  active === index ? "active-item" : null
+                }`}
+                key={index}
+                onClick={() => setActive(index)}
+              >
+                {item}
+              </div>
+            ))}
+
+            {/* <div className="menu-item">Home</div>
+            <div className="menu-item">New Application</div>
+            <div className="menu-item">Settings</div> */}
+          </div>
+        </div>
+      </div>
+
       <div className="footer">
-        Copyright @2021{" "}
-        <a href="/footer">Civil Aviation Authority of Bangladesh</a>
+        Copyright @2021 <span>Civil Aviation Authority of Bangladesh</span>
       </div>
     </div>
   );
