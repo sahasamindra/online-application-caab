41860965ca4076893f55328979266b938c3301ac
Samindra <samindra.saha@jhorotek.com>
2021-12-23 15:45:52 +0600

    layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,10 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header/Header";
 import "./layout.css";
+
+//Font awesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSun } from "@fortawesome/free-solid-svg-icons";
+import { faMoon } from "@fortawesome/free-solid-svg-icons";
 
 function Layout() {
   let menus = ["Home", "New Application", "Settings"];
   const [active, setActive] = useState(0);
+  const [clock, setClock] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => setClock(new Date()), 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
 
   return (
     <div className="global">
@@ -31,6 +44,29 @@ function Layout() {
             <div className="menu-item">New Application</div>
             <div className="menu-item">Settings</div> */}
           </div>
+        </div>
+        <div className="welcome">
+          {clock.getHours() < 18 ? (
+            <FontAwesomeIcon icon={faSun} className="icon-sun" />
+          ) : (
+            <FontAwesomeIcon icon={faMoon} className="icon-sun" />
+          )}
+          <br />
+          <span id="welcomeMsg">
+            Good{" "}
+            {clock.getHours() < 12
+              ? "Morning"
+              : clock.getHours() < 18
+              ? "Afternoon"
+              : "Evening"}
+          </span>
+          <br />
+          <span id="clock">
+            {clock.toLocaleTimeString()} |{" "}
+            {`${
+              clock.getMonth() + 1
+            }-${clock.getDate()}-${clock.getFullYear()}`}
+          </span>
         </div>
       </div>
 
