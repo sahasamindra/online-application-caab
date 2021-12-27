ff67f0246dc22ec159f798d1ce7b04e9217aef10
Samindra <samindra.saha@jhorotek.com>
2021-12-27 16:19:36 +0600

    user dashboard complete

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,15 +2,31 @@ import React from "react";
 import Header from "./Header/Header";
 import "./layout.css";
 import Logo from "../../assets/images/caabLogo.png";
+import Table from "react-bootstrap/Table";
+import SideBar from "./SideBar/SideBar";
 
 //progress bar
 import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 
-import SideBar from "./SideBar/SideBar";
+//font awesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronUp } from "@fortawesome/free-solid-svg-icons";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import { faDownload } from "@fortawesome/free-solid-svg-icons";
+import { faPrint } from "@fortawesome/free-solid-svg-icons";
+import { faArrowCircleRight } from "@fortawesome/free-solid-svg-icons";
 
 function Layout() {
   const percentage = 25;
+
+  const sortingIcon = (
+    <div className="table-header-icon-container">
+      <FontAwesomeIcon icon={faChevronUp} className="table-header-icon" />
+      <FontAwesomeIcon icon={faChevronDown} className="table-header-icon" />
+    </div>
+  );
+
   return (
     <div className="global">
       <div className="header-bar">
@@ -51,8 +67,8 @@ function Layout() {
               </div>
               <div className="profile-name">Complete Your Profile</div>
               <div className="profile-designation">
-                To successfully register your profile as an expert and to be
-                available in the search results.
+                To successfully register your profile as an expert and <br /> to
+                be available in the search results.
               </div>
             </div>
 
@@ -82,7 +98,133 @@ function Layout() {
             </div>
           </div>
 
-          <div className="history"></div>
+          <div className="history">
+            <div className="table-subheader">Overview</div>
+            <div className="table-header">History</div>
+            <Table hover className="overview-table">
+              <thead>
+                <tr className="table-row-header">
+                  <th>
+                    <div className="table-header-title">SN {sortingIcon}</div>
+                  </th>
+                  <th>
+                    <div className="table-header-title">
+                      Application Name {sortingIcon}
+                    </div>
+                  </th>
+                  <th>
+                    <div className="table-header-title">
+                      Application Number {sortingIcon}
+                    </div>
+                  </th>
+                  <th>
+                    <div className="table-header-title">
+                      Card Type {sortingIcon}
+                    </div>
+                  </th>
+                  <th>
+                    <div className="table-header-title">
+                      Card Validity {sortingIcon}
+                    </div>
+                  </th>
+                  <th>
+                    <div className="table-header-title">
+                      Status {sortingIcon}
+                    </div>
+                  </th>
+                  <th>
+                    <div className="table-header-title">Date {sortingIcon}</div>
+                  </th>
+                  <th>
+                    <div className="table-header-title">
+                      Action {sortingIcon}
+                    </div>
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="border">
+                <tr>
+                  <td>1</td>
+                  <td>Faisal Ahmed</td>
+                  <td>100065420</td>
+                  <td>NAP</td>
+                  <td>08-11-2022</td>
+                  <td className="draft">Draft</td>
+                  <td>08-11-2021</td>
+                  <td>
+                    <FontAwesomeIcon
+                      icon={faArrowCircleRight}
+                      className="table-icon"
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td>Faisal Ahmed</td>
+                  <td>100065420</td>
+                  <td>NAP</td>
+                  <td>08-11-2022</td>
+                  <td className="pending">Pending</td>
+                  <td>08-11-2021</td>
+                  <td>
+                    <FontAwesomeIcon icon={faPrint} className="table-icon" />
+                    <FontAwesomeIcon
+                      icon={faDownload}
+                      className="table-icon table-icon-margin-left"
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td>3</td>
+                  <td>Faisal Ahmed</td>
+                  <td>100065420</td>
+                  <td>NAP</td>
+                  <td>08-11-2022</td>
+                  <td className="accepted">Accepted</td>
+                  <td>08-11-2021</td>
+                  <td>
+                    <FontAwesomeIcon icon={faPrint} className="table-icon" />
+                    <FontAwesomeIcon
+                      icon={faDownload}
+                      className="table-icon table-icon-margin-left"
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td>4</td>
+                  <td>Faisal Ahmed</td>
+                  <td>100065420</td>
+                  <td>NAP</td>
+                  <td>08-11-2022</td>
+                  <td className="rejected">Rejected</td>
+                  <td>08-11-2021</td>
+                  <td>
+                    <FontAwesomeIcon icon={faPrint} className="table-icon" />
+                    <FontAwesomeIcon
+                      icon={faDownload}
+                      className="table-icon table-icon-margin-left"
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td>5</td>
+                  <td>Faisal Ahmed</td>
+                  <td>100065420</td>
+                  <td>NAP</td>
+                  <td>08-11-2022</td>
+                  <td className="pending">Pending</td>
+                  <td>08-11-2021</td>
+                  <td>
+                    <FontAwesomeIcon icon={faPrint} className="table-icon" />
+                    <FontAwesomeIcon
+                      icon={faDownload}
+                      className="table-icon table-icon-margin-left"
+                    />
+                  </td>
+                </tr>
+              </tbody>
+            </Table>
+          </div>
         </div>
       </div>
 
